5ad80ac21e51885610d81364265f466d1a4e609f
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-21 02:05:28 -0300

    addMiddleware

--- src/components/organizations/routes.ts
+++ src/components/organizations/routes.ts
@@ -1,30 +1,27 @@
 import { Request, Response } from 'express';
-import {
-  getById,
-  getOrganizations,
-  createOrganization,
-  patchOrgnization,
-  addUserToOrganization
-} from './service';
+import { getById, getOrganizations, createOrganization, patchOrgnization, addUserToOrganization } from './service';
 import { HttpStatusCode } from '../../commons/constants';
-import { Paths } from './utils'
+import { Paths } from './utils';
+import { authMiddleware } from 'ciencia-argentina-backend-commons';
+
 export default [
- {
-   path:Paths.Get,
-   method: 'get',
-   handler: [
-     async ({query}: Request, res: Response): Promise<void> => {
-       //console.log(res.locals.jwt)
-       const result = await getOrganizations(query);
-       res.status(HttpStatusCode.Ok).send(result)
-     }
-   ]
- },
+  {
+    path: Paths.Get,
+    method: 'get',
+    handler: [
+      async ({ query }: Request, res: Response): Promise<void> => {
+        const result = await getOrganizations(query);
+        res.status(HttpStatusCode.Ok).send(result);
+      },
+    ],
+  },
   {
     path: Paths.GetById,
     method: 'get',
     handler: [
+      authMiddleware('claim'),
       async ({ params }: Request, res: Response): Promise<void> => {
+        //console.log(res.locals.jwt)
         const result = await getById(params.id);
         res.status(HttpStatusCode.Ok).send(result);
       },
@@ -34,18 +31,18 @@ export default [
     path: Paths.Post,
     method: 'post',
     handler: [
-      async ( { body }: Request, res: Response): Promise<void> => {
+      async ({ body }: Request, res: Response): Promise<void> => {
         const result = await createOrganization(body);
         res.status(HttpStatusCode.Created).send(result);
       },
     ],
   },
   {
-    path: Paths.PostProfile,//TODO: Cruce de responsabilidades, mal, pero MVP
+    path: Paths.PostProfile, //TODO: Cruce de responsabilidades, mal, pero MVP
     method: 'post',
     handler: [
-      async ( { params,body }: Request, res: Response): Promise<void> => {
-        const result = await addUserToOrganization(params.id,body);
+      async ({ params, body }: Request, res: Response): Promise<void> => {
+        const result = await addUserToOrganization(params.id, body);
         res.status(HttpStatusCode.Created).send(result);
       },
     ],
@@ -54,7 +51,7 @@ export default [
     path: Paths.Patch,
     method: 'patch',
     handler: [
-      async ( req: Request, res: Response): Promise<void> => {
+      async (req: Request, res: Response): Promise<void> => {
         const result = await patchOrgnization(req.params.id, req.body);
         res.status(HttpStatusCode.Ok).send(result);
       },
