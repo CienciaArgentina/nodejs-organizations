a886f35fe475965affc1c80537f41ca1b4aed818
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2021-02-22 20:30:03 -0300

    delete address from jsonSchema

--- src/components/organizations/utils/schemas/postOrganizationBody.ts
+++ src/components/organizations/utils/schemas/postOrganizationBody.ts
@@ -1,28 +1,28 @@
-import { JSONSchema7 } from 'json-schema'
-import { Address } from './Address'
+import { JSONSchema7 } from 'json-schema';
+import { Address } from './Address';
 export const postOrganizationBody: JSONSchema7 = {
   type: 'object',
   properties: {
     name: {
       type: 'string',
-      minLength: 1
+      minLength: 1,
     },
     address: Address,
     acronym: {
-      type: ['string', 'null']
+      type: ['string', 'null'],
     },
     summary: {
-      type: ['string', 'null']
+      type: ['string', 'null'],
     },
     description: {
       type: ['string', 'null'],
       minLength: 5,
-      maxLength: 1000
+      maxLength: 1000,
     },
     website: {
       type: ['string', 'null'],
-      pattern: "^(?:https?:\/\/)([\da-z\.-]+\.[a-z\.]{2,6}|[\d\.]+)(?:[\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?$"
-    }
+      pattern: '^(?:https?://)([da-z.-]+.[a-z.]{2,6}|[d.]+)(?:[/:?=&#]{1}[da-z.-]+)*[/?]?$',
+    },
   },
-  required: ['name','address']
-}
+  required: ['name'],
+};
